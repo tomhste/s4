c2b26cd61f25f8d15203031ec8f56f45cff276bb
tomhste <tomhste@gmail.com>
2016-10-12 15:16:51 +0000

    SAP

---
+++ SAP.java
@@ -0,0 +1,48 @@
+package s4;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.*;
+
+
+
+public class SAP {
+
+
+    // constructor takes a digraph ( not necessarily a DAG )
+        public SAP ( Digraph G)
+        {
+
+        }
+
+         // length of shortest ancestral path between v and w; -1 if no such path
+        public int length ( int v , int w)
+        {
+            return 0;
+        }
+
+       // a shortest common common ancestor of v and w; -1 if no such path
+       public int ancestor ( int v , int w)
+        {
+        return 0;
+        }
+
+       // length of shortest ancestral path of vertex subsets A and B ; -1 if no such path
+       public int length ( Iterable < Integer > A , Iterable < Integer > B)
+        {
+        return 0;
+        }
+
+       // a shortest common ancestor of vertex subsets A and B; -1 if no such path
+       public int ancestor ( Iterable < Integer > A , Iterable < Integer > B)
+        {
+        return 0;
+        }
+
+       // do unit testing of this class
+       public static void main ( String [] args )
+        {
+
+
+        }
+
+}
